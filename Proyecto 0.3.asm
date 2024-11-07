5493958b58e93c131c980730e2e5f07e9d6ecc31
Leonardo <159551259+LeonardoUSB@users.noreply.github.com>
2024-11-06 21:00:43 -0400

    Update Proyecto 0.3.asm
    Funciones Extras

--- Proyecto 0.3.asm
+++ Proyecto 0.3.asm
@@ -8,15 +8,15 @@ Mes:.byte 0
 Dia:.byte 0
 
 #Uso de Registros
+# $a1 Auxiliar para loops
 # $t0 lleva la cuenta cuantas veces se ha presionado s. Comienza en 0
 # $t1 tendra la direccion de unos programas
-# $t4 lleva la cuenta de si es mañana o noce. O o 1
+# $t4 lleva la cuenta de si es maÃ±ana o noce. O o 1
 
 
 .text
 main: j Impresion
-     li $t0 0x004000ac
-     addi $sp, $sp, -4 
+     
 #Funciones Basicas
 Print_TZ:
 	beqz $t4, AM
@@ -43,15 +43,25 @@ Print_Ano:
 	lb $a0, Ano
 	li $v0, 1
 	syscall
+	jr $ra
 Print_Mes:
 	lb $a0, Mes
 	li $v0, 1
 	syscall
+	jr $ra
 Print_Dia:
 	lb $a0, Dia
 	li $v0, 1
 	syscall
-Print_Space:
+	jr $ra
+Print_Space:#Necesita cargar en las iteraciones
+	loop:   beqz $a1, endloop
+			li $a0, ' ' 
+			li $v0, 11
+			syscall
+			sub $a1, $a1,1
+	endloop:		
+	jr $ra
 Print_DP:
 Print_LB:
 	li $a0, '['
@@ -68,23 +78,91 @@ Guardar:
 	move $t1, $ra
 	add $t1, $t1, 12
 	jr $ra
+	
+RestartS:
+	li $t0 1
+	jr $ra
 #Funcion de Impresion General
 Impresion:
 
 	jal Guardar
 	move $ra, $t1
-	beq $t0, 0, Print_LB
-	jal Print_TZ
-	beq $t0, 0, Print_RB
-
 	beq $t0, 1, Print_LB
-	jal Print_Hora
+	
+	jal Print_TZ
+	
+	jal Guardar
+	move $ra, $t1
 	beq $t0, 1, Print_RB
 	
-
+	li $a1 2
+	jal Print_Space
+	
+	jal Guardar
+	move $ra, $t1
 	beq $t0, 2, Print_LB
-	jal Print_Min
+	
+	jal Print_Hora
+	
+	jal Guardar
+	move $ra, $t1
 	beq $t0, 2, Print_RB
+	
+	li $a1 2
+	jal Print_Space
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 3, Print_LB
+	
+	jal Print_Min
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 3, Print_RB
+	
+	li $a1 2
+	jal Print_Space
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 4, Print_LB
+	
+	jal Print_Ano
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 4, Print_RB
+	
+	li $a1 2
+	jal Print_Space
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 5, Print_LB
+	
+	jal Print_Mes
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 5, Print_RB
+	
+	li $a1 2
+	jal Print_Space
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 6, Print_LB
+	
+	jal Print_Dia
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 6, Print_RB
+	
+	jal Guardar
+	move $ra, $t1
+	beq $t0, 7, RestartS
 	
 	j end
 
