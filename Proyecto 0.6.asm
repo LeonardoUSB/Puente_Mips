2401d6bab5b43b66d51f4501909c5481e32e82eb
Alejandro De Vincenzo <148364766+Alentris@users.noreply.github.com>
2024-11-14 10:35:20 -0400

    Pilan't
    No más pila ;)

---
+++ Proyecto 0.6.asm
@@ -0,0 +1,444 @@
+.data
+nueva_linea: .asciiz "\n"
+espacio: .asciiz " "
+espacio1: .asciiz " "
+espacio2: .asciiz " "
+espacio3: .asciiz " "
+espacio4: .asciiz " "
+dd: .asciiz ":"
+guion1: .asciiz "-"
+guion2: .asciiz "-"
+formato: .asciiz "YEAR MO DD"
+mensaje: .asciiz "Ingrese un caracter: "
+teclado: .space 1
+TimeZone1: .asciiz "AM"
+TimeZone2: .asciiz "PM"
+Hora: .byte 12
+Minuto1: .byte 3
+Minuto2: .byte 0
+Ano: .word 2024
+Mes: .byte 11
+Dia: .byte 15
+
+#Uso de Registros
+#a0 Para SYSCALL
+
+# $t1, $t8 y $t9 se usan temporalmente para ir almacenando en registros algunas variables.
+# $t0 y $t2 son para la pila.
+# $t5-t7 lo usa Calendario.
+# $s6 es para guardar el teclado.
+# $s7 es el contador de casos para Set.
+
+# Estructura resumen
+# main:
+# 	Impresión (Se imprime en consola Cabecera, Medio, Calendario).
+#	Teclado (Se recibe por teclado los botones S,M,U,D). NO HECHO.
+#	Acción (Se modifican los valores de la fecha u hora, lo cual modificará la impresión). NO HECHO.
+#       j main
+
+# Nota: El código de todos los Print y de la pila está hacia el final.
+
+
+.text
+main:	
+	# Impresión
+	jal Impresion
+        
+	# Teclado
+	jal Teclado
+    
+	# Acción
+	j Accion
+
+Impresion:
+	j Cabecera
+Cabecera:
+	# Imprime lo siguiente: PM 12:00  2024-11-15
+
+	# Salto de línea.
+	li $v0 4            
+	la $a0 nueva_linea
+	syscall
+
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio1        
+ 	syscall
+
+	# PM
+        li $v0 4            
+        la $a0 TimeZone2
+        syscall
+
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio2        
+ 	syscall
+	
+	# Hora
+	la $t1 Hora
+	lb $t8, 0($t1)	# Carga Hora en $t8.
+	
+        li $v0 1          
+        move $a0 $t8
+	syscall
+	
+	# :
+        li $v0 4            
+        la $a0 dd
+        syscall
+	
+	# Minuto1
+	la $t1 Minuto1
+	lb $t8, 0($t1)	# Carga Minuto1 en $t8.
+		
+        li $v0 1          
+        move $a0 $t8
+	syscall
+	
+	# Minuto2
+	la $t1 Minuto2
+	lb $t8, 0($t1)	# Carga Minuto2 en $t8.
+	
+        li $v0 1          
+        move $a0 $t8
+	syscall
+	
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio3        
+ 	syscall	
+	
+	# Ano
+	la $t1 Ano
+	lw $t8, 0($t1)	# Carga Ano en $t8.
+		
+        li $v0 1          
+        move $a0 $t8
+	syscall														
+
+	# Guion1
+        li $v0 4            
+        la $a0 guion1
+        syscall
+
+	# Mes
+	la $t1 Mes
+	lb $t8, 0($t1)	# Carga Mes en $t8.
+		
+        li $v0 1          
+        move $a0 $t8
+	syscall
+	
+	# Guion2
+        li $v0 4            
+        la $a0 guion2
+        syscall	
+	
+	# Dia
+	la $t1 Dia
+	lb $t8, 0($t1)	# Carga Dia en $t8.
+		
+        li $v0 1          
+        move $a0 $t8
+	syscall
+	
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio4 
+ 	syscall
+
+ 	li $t1 0     # Vaciar $t1 por si acaso.
+	li $t8 0     # Vaciar $t8 por si acaso. 	
+ 	
+ 	j Medio
+
+Medio:
+	# Imprime lo siguiente: (10 espacios) YEAR MO DD
+
+	# Imprime una nueva línea para estar por debajo de Cabecera.
+	li $v0 4            
+	la $a0 nueva_linea        
+ 	syscall
+		
+	# Bucle que se ejecuta 10 veces para imprimir 10 espacios.		
+	li $t4 0
+	
+	j loop
+	
+loop:
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio        
+ 	syscall
+ 	
+	addi $t4 $t4 1
+		
+	blt $t4 10 loop
+	
+	# Cuando se han impreso 10 espacios se rompe el bucle y sigue aquí.
+	li $t4 0    # Vaciar $t4 por si acaso.
+	j Medio2
+
+Medio2:
+	# Imprimir YEAR MO DD.
+	li $v0 4            
+	la $a0 formato        
+ 	syscall
+ 	
+ 	# Imprime la nueva línea para el Calendario.
+	li $v0 4            
+	la $a0 nueva_linea        
+ 	syscall
+	
+	j Calendario
+
+Calendario:
+	# Esto no maneja pilas ni jal. Esto es puro código que se ejecuta de arriba a abajo.
+	
+    	li $t5 1            # El número de días. Empieza en el 1.
+    	li $t6 31           # La cantidad de días que va a tener el mes.
+    	li $t7 0            # Contador de días por semana.
+	
+printCalendario:	
+	bgt $t5 $t6 endCalendario     # Si $t5 = 31, ya imprimió todos los días.
+
+	blt $t5 10 unidades # Si el día es de un dígito (menor que 10), ir a unidades.
+    
+	bgt $t5 9 decenas   # Si el día es de dos dígitos (mayor que 9), ir a decenas.               
+
+
+unidades:
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio        
+ 	syscall
+    
+  	# Imprimir el día.
+	li $v0 1            
+	move $a0 $t5       
+	syscall
+
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio   
+	syscall
+    
+	addi $t5 $t5 1     # Incrementar el contador de días en 1.
+	addi $t7 $t7 1     # Incrementar el contador de días por semana en 1.
+    
+	beq $t7 7 salto2    # Si el contador de días por semana es igual a 7, ir a "salto" para generar un salto de línea.
+
+	j printCalendario
+
+decenas:
+	# Imprimir el día.
+	li $v0 1            
+	move $a0 $t5        
+	syscall
+
+	# Imprimir un espacio.
+	li $v0 4            
+	la $a0 espacio        
+	syscall
+
+	addi $t5 $t5 1     # Incrementar el contador de días en 1.
+	addi $t7 $t7 1     # Incrementar el contador de días por semana en 1.
+    
+	beq $t7 7 salto2    # Si el contador de días por semana es igual a 7, ir a "salto" para generar un salto de línea.
+
+	j printCalendario
+
+salto2:
+	li $v0 4            
+	la $a0 nueva_linea
+	syscall
+
+	li $t7 0           # Reiniciar el contador de días por semana en 0.
+	j printCalendario
+
+end:
+	li $v0, 10         # Finaliza el programa.
+	syscall
+	
+endCalendario:
+	jr $ra
+
+	
+Teclado:
+	# Salto de línea
+	li $v0 4            
+	la $a0 nueva_linea
+	syscall
+	
+	# Imprimir el mensaje 
+	li $v0, 4 
+	la $a0, mensaje 
+	syscall
+	
+	li $v0 8
+	la $a0 teclado
+	li $a1 2
+	syscall
+	
+	la $t8 teclado
+	lb $s6 0($t8)   # Se guarada en $s6 el botón del teclado.
+	
+	jr $ra
+	
+Accion:
+	# S: Set
+	li $t8 'S'
+	beq $s6 $t8 Set  # Si la entrada por teclado es S, ir a Set.
+	
+	j main
+
+Set:
+	addi $s7 $s7 1   # $s7 es el contador de casos de Set. 
+	                 # La primera vez inicia en 0 y al llegar aquí, suma 1 y cae en el caso 1.
+	
+	# Caso 1
+	beq $s7 1 Caso1
+	
+	# Caso 2
+	beq $s7 2 Caso2
+
+	# Caso 3
+	beq $s7 3 Caso3
+
+	# Caso 4
+	beq $s7 4 Caso4										
+	
+	# Caso 5
+	beq $s7 5 Caso5
+	
+	# Caso 6
+	beq $s7 6 Caso6			
+		
+Caso1:
+	# Devolver a la normalidad el Caso6.
+	la $t8 guion2
+	li $t9 45
+	sb $t9 0($t8)
+	
+	la $t8 espacio4
+	li $t9 32
+	sb $t9 0($t8)	
+	
+	# Modificar espacio1 y espacio2
+	la $t8 espacio1
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 espacio2
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	j main	
+	
+Caso2:	
+	# Devolver a la normalidad el Caso1.
+	la $t8 espacio1
+	li $t9 32
+	sb $t9 0($t8)
+	
+	# Modificar espacio2 y dd.
+	la $t8 espacio2
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 dd
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	j main
+Caso3:
+	# Devolver a la normalidad el Caso2.
+	la $t8 espacio2
+	li $t9 32
+	sb $t9 0($t8)
+	
+	# Modificar dd y espacio3.
+	la $t8 dd
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 espacio3
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	j main
+
+Caso4:
+	# Devolver a la normalidad el Caso3.
+	la $t8 dd
+	li $t9 58
+	sb $t9 0($t8)
+	
+	# Modificar espacio3 y guion1.
+	la $t8 espacio3
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 guion1
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	j main
+Caso5:
+	# Devolver a la normalidad el Caso4.
+	la $t8 espacio3
+	li $t9 32
+	sb $t9 0($t8)
+	
+	# Modificar guion1 y guion2.
+	la $t8 guion1
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 guion2
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	j main
+Caso6:
+	# Devolver a la normalidad el Caso5.
+	la $t8 guion1
+	li $t9 45
+	sb $t9 0($t8)
+	
+	# Modificar guion1 y guion2.
+	la $t8 guion2
+	li $t9 91
+	sb $t9 0($t8)
+	
+	la $t8 espacio4
+	li $t9 93
+	sb $t9 0($t8)	
+	
+	li $s7 0
+	j main
+	
+	
+# PILA
+	
+guardarPila:
+	move $t2,$ra
+	add $t2, $t2, 8 
+	sw $t2 0($sp)
+	add $sp, $sp, 4
+	add $t0, $t0, 1
+	li $t2, 0
+	jr $ra
+usarPila:
+	sub $t0, $t0, 1
+	sub $sp, $sp, 4
+	lw $ra, 0($sp)
+	jr $ra
+eliminarPila:
+	beqz $t0, salto
+	sub $t0, $t0, 1
+	sub $sp, $sp, 4
+	jr $ra
+	salto:
+		jr $ra	
